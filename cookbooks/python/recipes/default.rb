56774cabfd25d090da23935da6fbc60ee891aec1
manibrar <manvirbrar1988@gmail.com>
2018-08-01 15:38:47 +0100

    blah

---
+++ cookbooks/python/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: python
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+package 'python'
+package 'python-pip'
+package 'python-flask'
+package 'libncurses5-dev'
